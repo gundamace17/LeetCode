024b951d8ea546a49a9042ac2b9fb2abdc0496c1
RileyL <51372465+gundamace17@users.noreply.github.com>
2020-10-05 11:25:16 -0700

    Update 0171. Excel Sheet Column Number.py

--- Python/0171. Excel Sheet Column Number.py
+++ Python/0171. Excel Sheet Column Number.py
@@ -7,16 +7,16 @@ Created on Mon Oct  5 11:15:35 2020
 
 class Solution:
     def titleToNumber(self, s):
-        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        pairs = {}
+        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # Create a A to Z string
+        pairs = {}  # Empty dictionary for storing the value corresponding to each letter
         for num in range(len(letters)):
             pairs[letters[num]] = (num + 1)
         print(pairs)
     
-        result = 0
-        i = len(s)
+        result = 0 # Initialize the result value
+        i = len(s) 
         while i >= 1:
-            result = result + pairs[s[i-1]]*pow(26, len(s) - i)
+            result = result + pairs[s[i-1]]*pow(26, len(s) - i) # like a decimal system, this case is like 26 system
             print(result)
             i -= 1
         return(result)
