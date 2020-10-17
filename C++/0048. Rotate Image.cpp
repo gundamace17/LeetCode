cb27c12fce239ef1ec32573b4740667ab9d72cb1
RileyL <51372465+gundamace17@users.noreply.github.com>
2020-10-16 19:40:20 -0700

    Add files via upload

---
+++ C++/0048. Rotate Image.cpp
@@ -0,0 +1,38 @@
+// 0048. Rotate Image.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        std::reverse(matrix.begin(), matrix.end());
+
+        for (int i = 0; i < matrix.size(); i++)
+            for (int j = i; j < matrix[0].size(); j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<vector<int>> A = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+    sol.rotate(A);
+
+    for (auto i = 0; i < A.size(); ++i)
+    {
+        for (auto j = 0; j < A[0].size(); ++j)
+        {
+            cout << A[i][j];
+        }
+        cout << endl;
+    }
+}
+
+
