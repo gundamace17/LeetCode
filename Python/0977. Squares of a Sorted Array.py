eb9e4fe968773439830f8e57e0c2cd109a5ae6cf
RileyL <51372465+gundamace17@users.noreply.github.com>
2020-10-18 16:29:35 -0700

    Update and rename 977. Squares of a Sorted Array.py to 0977. Squares of a Sorted Array.py

--- Python/0977. Squares of a Sorted Array.py
+++ Python/0977. Squares of a Sorted Array.py
@@ -5,6 +5,10 @@ Created on Sun Oct 18 16:24:09 2020
 @author: gunda
 """
 
+# Explanation of the Algorithm:
+# 1. Square each element in A
+# 2. Sort A
+
 class Solution:
     def sortedSquares(self, A):
         
